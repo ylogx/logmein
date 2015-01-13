f80daeab2d56f4171eb017eb98f117d4bf9dd6b2
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-01-13 18:18:53 +0530

    Ready for PyPi upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+add_keywords = dict(
+    entry_points = {
+        'console_scripts': ['logmein = logmein.logmein:main'],
+    },
+)
+
+setup(
+        name='LogMeIn',
+        description='Log in to networks',
+        version='0.0.1',
+        packages=['logmein'],
+        license='GPLv3+',
+        author='Shubham Chaudhary',
+        author_email='me@shubhamchaudhary.in',
+        url='https://github.com/shubhamchaudhary/logmein',
+        long_description=open('README.txt').read(),
+        **add_keywords
+)
+
