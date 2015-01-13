12aafbc63b8f1df28badcdd465bc382b616bbeb7
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-01-13 18:46:20 +0530

    Separate out network functionality

--- logmein/main.py
+++ logmein/main.py
@@ -22,93 +22,20 @@ import atexit
 import os
 import platform
 import random
-import re
 import sys
 import time
 
 from argparse import ArgumentParser
-from logmein.statuscode import StatusCode
-from logmein.fileparser import parse_file_for_credential
 
 if sys.version_info >= (3,):
-    import urllib.request as urllib2
-    import urllib.parse as urlparse
     import urllib.error as urlerror
 else:
-    import urllib2
-    import urlparse
+    import urlerror
 
-def login_pucampus(username, password):
-    ''' Perform login '''
-    url = 'https://securelogin.arubanetworks.com/cgi-bin/login?cmd=login'
-    values = {'user' : username,
-              'password' : password}
-    # Create request
-    data = urlparse.urlencode(values)
-    data = data.encode('utf-8')
-    req = urllib2.Request(url, data)
-    # Send request
-    try:
-        response = urllib2.urlopen(req)         #res.geturl(), .url=str, .status=200, .info=200, .msg=OK,
-    except urlerror.HTTPError as exep:
-        print('The server couldn\'t fulfill the request.',
-              'Error code: ', exep.code)
-        print('You\'re probably logged in!')
-        return StatusCode.LOGGED_IN
-    except urlerror.URLError as exep:
-        print('We failed to reach a server.')
-        print('Reason: ', exep.reason)
-        return StatusCode.CONNECTION_ERROR
-    else:
-        # everything is fine
-        the_page = str(response.read()) # More pythonic than .decode('utf-8')
-        # Parse for success or failure
-        match = re.search('Authentication failed', the_page)
-        if match:
-            print('Authentication failed.')
-            print('Maybe your username or password is wrong.')
-            return StatusCode.AUTH_ERROR
-        success = re.search('External Welcome Page', the_page)
-        if success:
-            print('Authentication Success. You\'re logged in')
-            return StatusCode.SUCCESS
-        if re.search('Only one user login session is allowed', the_page):
-            print('Only one user login session is allowed')
-            return StatusCode.MULTIPLE_LOGIN
-
-def logout_pucampus():
-    ''' Perform logout '''
-    print('Sending logout request')
-#     url = 'http://172.16.4.204/cgi-bin/login'
-#     data = urlparse.urlencode({'cmd' : 'logout' })
-#     full_url = url + '?' + data
-#     req = urllib2.Request(full_url)         #req.full_url,
-    # Send request
-    try:
-        #response = urllib2.urlopen(full_url)
-        #res.geturl(), .url=str, .status=200, .info=200, .msg=OK,
-        response = urllib2.urlopen(
-                'https://securelogin.arubanetworks.com/cgi-bin/login?cmd=logout'
-                )
-    except urlerror.HTTPError as exep:
-        print('The server couldn\'t fulfill the request.',
-              'Error code: ', exep.code)
-    except urlerror.URLError as exep:
-        print('We failed to reach a server.')
-        print('Reason: ', exep.reason)
-    else:
-        # everything is fine
-        the_page = str(response.read()) # More pythonic than .decode('utf-8')
-        # Parse for success or failure
-        if re.search('Logout', the_page):
-            print('Logout successful')
-            return StatusCode.SUCCESS
-        elif re.search('User not logged in', the_page):
-            print('You\'re not logged in')
-            return StatusCode.SUCCESS
-        else:
-            print(the_page)
-            return StatusCode.UNKNOWN_ERROR
+from logmein.fileparser import parse_file_for_credential
+from logmein.network import login_pucampus
+from logmein.network import logout_pucampus
+from logmein.statuscode import StatusCode
 
 def print_usage():
     ''' Simpler usage message '''
@@ -134,8 +61,17 @@ def print_help():
     for line in text:
         print(line)
 
+def stop_for_windows():
+    ''' If on windows platform, stop and wait for input
+    '''
+    if os.name == 'nt' or platform.system() == 'Windows':
+        input('Press Enter or Close the window to exit !')
+
 def main():
     ''' Main function '''
+    # Run this function everytime on exit
+    atexit.register(stop_for_windows)
+
     default_credential_files = [
         os.path.join(os.path.expanduser('~'), '.login.txt'),
         os.path.join(os.path.expanduser('~'), 'login.txt'),
@@ -227,15 +163,7 @@ def do_login(username, password):
     except:
         raise
 
-def stop_for_windows():
-    ''' If on windows platform, stop and wait for input
-    '''
-    if os.name == 'nt' or platform.system() == 'Windows':
-        input('Press Enter or Close the window to exit !')
-
 if __name__ == '__main__':
-    # Run this function everytime on exit
-    atexit.register(stop_for_windows)
     try:
         return_code = main()
         sys.exit(return_code)
---
+++ logmein/network.py
@@ -0,0 +1,86 @@
+import re
+import sys
+
+from logmein.statuscode import StatusCode
+
+if sys.version_info >= (3,):
+    import urllib.request as urllib2
+    import urllib.parse as urlparse
+    import urllib.error as urlerror
+else:
+    import urllib2
+    import urlparse
+
+
+def login_pucampus(username, password):
+    ''' Perform login '''
+    url = 'https://securelogin.arubanetworks.com/cgi-bin/login?cmd=login'
+    values = {'user' : username,
+              'password' : password}
+    # Create request
+    data = urlparse.urlencode(values)
+    data = data.encode('utf-8')
+    req = urllib2.Request(url, data)
+    # Send request
+    try:
+        response = urllib2.urlopen(req)         #res.geturl(), .url=str, .status=200, .info=200, .msg=OK,
+    except urlerror.HTTPError as exep:
+        print('The server couldn\'t fulfill the request.',
+              'Error code: ', exep.code)
+        print('You\'re probably logged in!')
+        return StatusCode.LOGGED_IN
+    except urlerror.URLError as exep:
+        print('We failed to reach a server.')
+        print('Reason: ', exep.reason)
+        return StatusCode.CONNECTION_ERROR
+    else:
+        # everything is fine
+        the_page = str(response.read()) # More pythonic than .decode('utf-8')
+        # Parse for success or failure
+        match = re.search('Authentication failed', the_page)
+        if match:
+            print('Authentication failed.')
+            print('Maybe your username or password is wrong.')
+            return StatusCode.AUTH_ERROR
+        success = re.search('External Welcome Page', the_page)
+        if success:
+            print('Authentication Success. You\'re logged in')
+            return StatusCode.SUCCESS
+        if re.search('Only one user login session is allowed', the_page):
+            print('Only one user login session is allowed')
+            return StatusCode.MULTIPLE_LOGIN
+
+def logout_pucampus():
+    ''' Perform logout '''
+    print('Sending logout request')
+#     url = 'http://172.16.4.204/cgi-bin/login'
+#     data = urlparse.urlencode({'cmd' : 'logout' })
+#     full_url = url + '?' + data
+#     req = urllib2.Request(full_url)         #req.full_url,
+    # Send request
+    try:
+        #response = urllib2.urlopen(full_url)
+        #res.geturl(), .url=str, .status=200, .info=200, .msg=OK,
+        response = urllib2.urlopen(
+                'https://securelogin.arubanetworks.com/cgi-bin/login?cmd=logout'
+                )
+    except urlerror.HTTPError as exep:
+        print('The server couldn\'t fulfill the request.',
+              'Error code: ', exep.code)
+    except urlerror.URLError as exep:
+        print('We failed to reach a server.')
+        print('Reason: ', exep.reason)
+    else:
+        # everything is fine
+        the_page = str(response.read()) # More pythonic than .decode('utf-8')
+        # Parse for success or failure
+        if re.search('Logout', the_page):
+            print('Logout successful')
+            return StatusCode.SUCCESS
+        elif re.search('User not logged in', the_page):
+            print('You\'re not logged in')
+            return StatusCode.SUCCESS
+        else:
+            print(the_page)
+            return StatusCode.UNKNOWN_ERROR
+
